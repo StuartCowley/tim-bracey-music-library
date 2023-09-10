27e8c015177af69df485007754761028957a92d7
Tim <50870607+toothless13@users.noreply.github.com>
2023-09-10 19:11:57 +0100

    start writing read artists test

---
+++ tests/artist-read.test.js
@@ -0,0 +1,8 @@
+const { expect } = require('chai');
+const request = require('supertest');
+const db = require('../src/db');
+const app = require('../src/app');
+
+describe('Read Artists', () => {
+  
+});
